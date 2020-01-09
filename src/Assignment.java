31d36400cf2bb0f37a4d53e9b5a6168be2515b16
rktyagi <rtyagi.vc@primerevenue.com>
2020-01-09 16:47:34 +0530

    new code

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -6,6 +7,28 @@ public class Assignment {
 	{
 		System.setProperty("webdriver.chrome.driver", ".//Driver//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.flipkart.com/");
+		driver.get("https://staging.engineer.ai/home");
+		driver.findElement(By.xpath("//div[@class='currencySelBox']/div[7]")).click();
+		driver.findElement(By.xpath("//button[Contains(Text(), ' Sign In ')]"));
+		//driver.findElement(By.xpath("//strong[Contains(Text(), ' Register ')]"));
+		driver.findElement(By.xpath("//div[@class='user-txt'][contains(text(),' Don’t have an account? ']")).click();
+
+		driver.findElement(By.name("signup_email")).sendKeys("abc@yopmail.com");
+		driver.findElement(By.xpath("//button[contains(text(), 'Next']")).click();
+
+		driver.findElement(By.name("firstlastname")).sendKeys("gfdghhfg");
+		driver.findElement(By.xpath("//input[@placeholder='Mobile Number']")).sendKeys("324243435");
+
+		driver.findElement(By.name("organisationName")).sendKeys("Navyug");
+		driver.findElement(By.name("signup_password")).sendKeys("New1life");
+
+		driver.findElement(By.xpath("@id='businesspurposes')/label")).click();
+		driver.findElement(By.className("submitButton")).click();
+
+		driver.findElement(By.className("doneButton")).click();
+		String username=driver.findElement(By.xpath("//div[@class='userPanel']/h3/strong")).getText();
+		
+		
+		
 	}
 }
